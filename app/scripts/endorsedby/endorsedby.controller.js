6f8fd7a5155a3a2ac8daf2e5e93a9b90fcc3b6f5
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2016-07-14 20:24:38 +0200

    Fill endorsers from json

--- app/scripts/endorsedby/endorsedby.controller.js
+++ app/scripts/endorsedby/endorsedby.controller.js
@@ -1,8 +1,28 @@
 (function() {
   'use strict';
 
-  function EndorsedbyController() {
-    this.endorsers = ['All', 'NLeSC', 'Meertens'];
+  function EndorsedbyController($attrs, $stateParams, $state, NdxHelperFunctions, NdxService) {
+    var jsonArrayFieldToChart = $attrs.jsonArrayFieldToChart;
+    var ndxInstanceName = $attrs.ndxServiceName;
+    var dimension = NdxHelperFunctions.buildDimensionWithArrayProperty(ndxInstanceName, jsonArrayFieldToChart);
+    var group = NdxHelperFunctions.buildGroupWithArrayProperty(dimension, jsonArrayFieldToChart);
+
+    this.endorsers = [];
+
+    this.isActive = function(endorser) {
+      return endorser === $stateParams.endorser;
+    };
+    this.go = function(endorser) {
+      $state.go('.', {endorser: endorser});
+    };
+
+    this.fillEndorsers = function() {
+      this.endorsers = ['All'].concat(group.top(Infinity).map(function(d) {
+        return d.key;
+      }));
+    };
+
+    NdxService.ready.then(this.fillEndorsers.bind(this));
   }
 
   angular.module('estepApp.endorsedby').controller('EndorsedbyController', EndorsedbyController);
