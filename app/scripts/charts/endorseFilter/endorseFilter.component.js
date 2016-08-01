adfe68fbcbed3bc986f0da913c768fc8691cedc8
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2016-08-01 15:21:50 +0200

    Use bag filter to filter on endorser.

--- app/scripts/charts/endorseFilter/endorseFilter.component.js
+++ app/scripts/charts/endorseFilter/endorseFilter.component.js
@@ -2,19 +2,20 @@
   'use strict';
 
   function EndorseFilterController($attrs, $state, NdxHelperFunctions) {
-    var ctrl = this;
     var ndxInstanceName = $attrs.ndxServiceName;
     var jsonArrayFieldToChart = 'inGroup';
     var stateFieldName = 'endorser';
     var dimension = NdxHelperFunctions.buildDimensionWithArrayProperty(ndxInstanceName, jsonArrayFieldToChart);
+
     if (ndxInstanceName === $state.$current.name &&
       stateFieldName in $state.params &&
       $state.params[stateFieldName]) {
       var filter = $state.params[stateFieldName];
+
       if (filter === 'All') {
         dimension.filterAll();
       } else {
-        dimension.filter(filter);
+        NdxHelperFunctions.bagFilterHandler()(dimension, [filter]);
       }
     }
   }
